4e61ccf8f92342bab94646fc44335a330f342c38
Ryan Crosser <ryankcrosser@gmail.com>
2018-08-07 15:08:15 -0400

    init commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+import Raven from "raven-js";
+import RavenVue from "raven-js/plugins/vue";
+
+Raven.config("https://7b925295c74e4fc68597d1860c5e0db0@sentry.io/1258026")
+  .addPlugin(RavenVue, Vue)
+  .install();
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
